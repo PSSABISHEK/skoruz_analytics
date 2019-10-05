0148ca32202caf48cfe7abbaeed204516c801c72
P.S.S.ABISHEK <pssabishek18@gmail.com>
2019-10-05 19:21:08 +0530

    Env Added

--- data_import/data_import.py
+++ data_import/data_import.py
@@ -1,14 +1,18 @@
 import os
 import sys
-
+from dotenv import load_dotenv
 import jaydebeapi
+
+load_dotenv()
 
 
 def load_data():
-    remote_filepath = r"F:\Internship\Skoruz\HDFS\Simba_Hive_JDBC\SIMBAHiveJDBC41.jar"
-    local_server = "172.16.1.30"
-    local_filepath = r"/home/prd_user/SIMBAHiveJDBC41.jar"
-    remote_server = "india.skoruz.com"
+    remote_filepath = os.getenv("PSS_PATH")
+    local_server = os.getenv("SKORUZ_IP")
+    local_filepath = os.getenv("SKORUZ_PATH")
+    remote_server = os.getenv("SKORUZ_WEBSITE")
+    server_port = os.getenv("SKORUZ_PORT")
+    server_database = os.getenv("SKORUZ_DB")
 
     filename = sys.argv[1]
     filename = os.path.splitext(filename)[0]
@@ -16,15 +20,14 @@ def load_data():
     f = open(data_dir, "r")
     datastore = f.read()
     try:
-        conn = jaydebeapi.connect("com.simba.hive.jdbc41.HS2Driver", "jdbc:hive2://" + local_server + ":10000/copy",
-                                  {'user': "hive", 'password': ""}, local_filepath)
+        conn = jaydebeapi.connect("com.simba.hive.jdbc41.HS2Driver",
+                                  "jdbc:hive2://" + remote_server + ":" + server_port +
+                                  "/" + server_database, {'user': "hive", 'password': ""}, remote_filepath)
         curs = conn.cursor()
         curs.execute('CREATE TABLE ' + filename + ' (str STRING) STORED AS ORC')
         curs.execute('INSERT INTO ' + filename + ' VALUES (\'' + datastore + '\')')
     except Exception:
         print("Error")
-    finally:
-        conn.close()
 
 
 load_data()
